82999cc95894077a83efa659c26a832e67f7fe20
Eben <ebenhafkamp@gmail.com>
2015-09-06 23:49:49 +1200

    sunday

--- app/js/main.js
+++ app/js/main.js
@@ -1,22 +1,32 @@
 $(function() {
-    
+
     var isOpen = false;
 
     $('#bars').on('click', function() {
 
         if(isOpen == true) {
-        
-            $('.sidebar').animate({top: '-11em'}, 1000, function() {})
-            
+
+            $('.sidebar').animate({top: '-11em'}, 600, function() {})
+
             isOpen = false;
-        
+
         } else {
-            
-             $('.sidebar').animate({top: '7em'}, 1000, function() {})
-            
+
+             $('.sidebar').animate({top: '7em'}, 300, function() {})
+
              isOpen = true;
-             
+
         };
-        
+
     });
+    $('#block').on('click', function() {
+      $(this).addClass('.expand');
+    });
+
+    setTimeout(function(){
+        $('.mouse').addClass('show').addClass('animated fadeIn');
+    }, 2000);
+    setTimeout(function(){
+        $('.mouse').addClass('animated fadeOut');
+    }, 6000);
 });
