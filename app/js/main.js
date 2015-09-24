81569fa5b8f48969d7fffcbed1744c816abcbe73
Eben <ebenhafkamp@gmail.com>
2015-09-25 00:43:55 +1200

    thurs night

--- app/js/main.js
+++ app/js/main.js
@@ -59,7 +59,7 @@ $(function() {
     $('.slider-for').slick({
       slidesToShow: 1,
       slidesToScroll: 1,
-      arrows: false,
+      arrows: true,
       fade: true,
       asNavFor: '.slider-nav'
     });
